fb3611b99d4eadc7834b8520369cfff0e2a7a3ce
akash-kd <39560107+akash-kd@users.noreply.github.com>
2024-01-27 21:13:50 +0530

    Chore: Changes

--- src/config/APIs/chron/index.js
+++ src/config/APIs/chron/index.js
@@ -1,12 +1,11 @@
 import { instance } from "..";
-import axios from "axios";
 
 export async function addChron(body) {
-  axios.post("http://localhost:4193/api/v1/chron/add", body);
+  instance.post("/chron/add", body);
 
   // const a = instance.post("/chron/add", );
 }
 
 export async function getAllChron() {
-  return axios.get("http://localhost:4193/api/v1/chron/get");
+  return instance.get("/chron/get");
 }
