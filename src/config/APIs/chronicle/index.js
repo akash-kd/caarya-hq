439618be0aefbdf9fbb97f0aaf1d3557ff89654e
akash-kd <39560107+akash-kd@users.noreply.github.com>
2024-01-31 18:07:14 +0530

    Chore: Changes

--- src/config/APIs/chronicle/index.js
+++ src/config/APIs/chronicle/index.js
@@ -1,17 +1,17 @@
 import { instance } from "..";
 
 export async function addChronicle(body) {
-  return instance.post("hq/chron", body);
+  return instance.post("hq/chronicle", body);
 }
 
 export async function getAllChronicle(query) {
-  return instance.get(`hq/chron?${new URLSearchParams(query)}`);
+  return instance.get(`hq/chronicle?${new URLSearchParams(query)}`);
 }
 
 export async function deleteChronicle(id) {
-  return instance.delete(`/hq/chron/${id}`);
+  return instance.delete(`/hq/chronicle/${id}`);
 }
 
 export async function updateChronicle(id) {
-  return instance.patch(`/hq/chron/${id}`);
+  return instance.patch(`/hq/chronicle/${id}`);
 }
